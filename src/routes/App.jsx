0e153ba2a206ae9bd1cf0a4ce9255b42f42a1e59
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-08-17 11:34:12 -0300

    Creada la estructura de carpetas y los archivos base para la app

---
+++ src/routes/App.jsx
@@ -0,0 +1,17 @@
+
+import { Home } from "./Home";
+import { Movies } from "./Movies";
+import { Series } from "./Series";
+
+export function App() {
+
+  return (
+    <>
+    Hola mundo
+      <Home />
+      <Movies />
+      <Series />
+    </>
+  )
+}
+
---
+++ src/routes/Series.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Series(props) {
+  return (
+    <div>
+      series
+    </div>
+  );
+}
+
