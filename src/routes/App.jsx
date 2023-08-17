d841ea0f0e3f21db9ecc823ef07eb02f2057ac1f
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-08-17 16:03:00 -0300

    Creada la lógica para el use context

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,3 +1,5 @@
+//Importación del contexto
+import { DataContextProvider } from "../dataContext/DataContext";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./Home";
@@ -7,11 +9,13 @@ import { Series } from "./Series";
 export function App() {
   return (
     <BrowserRouter>
+    <DataContextProvider>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/movies" element={<Movies />}/>
         <Route path="/series" element={<Series />}/>
       </Routes>
+    </DataContextProvider>
     </BrowserRouter>
   )
 }
