22410df8e38b6b8900ab923312fa33045989dd7c
fspind71 <fspind@gmail.com>
2023-08-22 17:21:53 -0300

    Renderizacion del array de Movies

--- src/routes/Movies.jsx
+++ src/routes/Movies.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CategoryCard } from "../components/CategoryCard";
+import { DataContext } from "../dataContext/DataContext";
 
 export function Movies(props) {
-  return <div>movies</div>;
+  const {validator, movies} = useContext(DataContext);
+  console.log(movies)
+  return (
+    <div>
+    {movies.map(index =>(
+      <CategoryCard
+        title={index.title}
+        img={index.images['Poster Art'].url}
+        key={index.title}
+      />
+    ))}
+
+    </div>
+  );
 }
