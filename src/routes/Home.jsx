5ae271576dceef5a2cd6eba3f51019635f2368d5
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2023-08-22 14:27:19 -0300

    Preparando push a facu

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -8,7 +8,6 @@ const imgMovies = "https://media.istockphoto.com/id/1191001701/photo/popcorn-and
 
 const imgSeries = 'https://www.latercera.com/resizer/gXOdyP_YUsjQ2MC6U9sUOv-gRxk=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/ONNUKFN3SNHK7F4KEH2BXHHHUA.jpg';
 
-
 export function Home() {
   const { 
     validator, 
