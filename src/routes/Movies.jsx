9b36ab867b783c81b4ab12a68d7d8f6e3b693292
fspind71 <fspind@gmail.com>
2023-08-24 16:51:15 -0300

    correcciones en menu hamburgesa. Logica en la vista movies. link para volver al home

--- src/routes/Movies.jsx
+++ src/routes/Movies.jsx
@@ -3,11 +3,33 @@ import { CategoryCard } from "../components/CategoryCard";
 import { Error } from "../components/Error";
 import { Loader } from "../components/Loader";
 import { DataContext } from "../dataContext/DataContext";
+import { Modal } from "../components/Modal";
 
 import "../styles/movie.css";
 
 export function Movies(props) {
   const {error, validator, movies} = useContext(DataContext);
+  const [seeModal, setSeeModal] = useState(false)
+
+  const [modalState, setModalState] = useState({
+    title: "",
+    img: "",
+    description: "",
+    releaseYear: 0,
+  });
+
+  function filterToModal(title) {
+    const movieFiltered = movies.find(movie => movie.title == title);
+    
+    setModalState({
+      title: movieFiltered.title,
+      img: movieFiltered.images["Poster Art"].url,
+      description: movieFiltered.description,
+      releaseYear: movieFiltered.releaseYear,
+    })
+    setSeeModal(true)
+  }
+
   
   return (
     <div className="home-container">
@@ -20,12 +42,20 @@ export function Movies(props) {
       {
         (validator && !error) && movies.map(index =>(
           <CategoryCard
+            filterToModal={filterToModal}
             title={index.title}
             img={index.images['Poster Art'].url}
             key={index.title}
           />
-        ))
-      }
+        ))}
+        <Modal
+        seeModal={seeModal}
+        setSeeModal={setSeeModal}
+        title={modalState.title}
+        img={modalState.img}
+        description={modalState.description}
+        releaseYear={modalState.releaseYear}
+      />
     </div>
   );
 }
