dc3bd9c8fb226a15ec87a4e82425b24e44dfb245
Marlene <marlene.nogara@revalcodesolution.com>
2023-08-24 15:42:57 -0300

    Maquetacion y estelizado de componente Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,14 +2,24 @@
 import React from 'react'
 import "../styles/Modal.css"
 
-export function Modal(){
-return (
-  <div>Modal
-    <h2>titulo</h2>
-    <img src="" alt="" />
-    <p>descripcion</p>
-    <span>año</span>
-     </div>
+export function Modal({ title, img, description, releaseYear }) {
+
+  return (
+    <div className='modal-container'>
+      <span className='exit-btn'>x</span>
+      <div className='modal-container-card'>
+        <img className="modal-container-img" src={img} alt="" />
+        <div className='modal-container-title'>
+          <h2 >{title}</h2>
+          <span>{releaseYear}</span>
+        </div>
+      </div>
+      <p className='modal-container-text'>{description}</p>
+
+    </div>
+
+
+
 
   )
 }
