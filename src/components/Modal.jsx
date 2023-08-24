1d299ecff05f94cfdaba032198caeb45fccf02e5
Marlene <marlene.nogara@revalcodesolution.com>
2023-08-24 10:09:33 -0300

    creacion de componentes Modal.jsx y Modal.css

---
+++ src/components/Modal.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import "../styles/Modal.css"
+
+export function Modal(){
+return (
+  <div>Modal
+    <h2>titulo</h2>
+    <img src="" alt="" />
+    <p>descripcion</p>
+    <span>año</span>
+     </div>
+
+  )
+}
