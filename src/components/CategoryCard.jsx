c1db3136609d8b95a07781b24632d621cf6b72ee
fspind71 <fspind@gmail.com>
2023-08-24 15:54:35 -0300

    Se agrega la logica al mvies.jsx

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -3,15 +3,20 @@ import '../styles/Category.css';
 
 import { Link } from 'react-router-dom'
 
-export function CategoryCard({img, title, name}) {
+export function CategoryCard({img, title, name, filterToModal}) {
   return (
     <Link 
     to={name} 
     className='category-card-container'
     >
       <img src={img} alt="" className='category-img'/>
-      {name && <p className="category-name">{name}</p>}
-      <h2 className='category-title'>{title}</h2>
+      {name && <p className="category-name">
+        {name}
+        </p>}
+      <h2 className='category-title'
+      onClick={()=>filterToModal(title)
+      }>
+        {title}</h2>
     </Link>
   );
 }
--- src/routes/Movies.jsx
+++ src/routes/Movies.jsx
@@ -1,21 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CategoryCard } from "../components/CategoryCard";
 import { DataContext } from "../dataContext/DataContext";
-import '../styles/movie.css'
+import "../styles/movie.css";
 
 export function Movies(props) {
-  const {validator, movies} = useContext(DataContext);
-  console.log(movies)
+  const { validator, movies } = useContext(DataContext);
+  const [modalState, setModalState] = useState({
+    title: "",
+    img: "",
+    description: "",
+    year: 0,
+  });
+  console.log(movies, modalState);
+
+  function filterToModal(title) {
+    const movieFiltered = movies.find((movie) => movie.title == title);
+    console.log({ movies, modalState });
+    setModalState({
+      title: movieFiltered.title,
+      img: movieFiltered.images["Poster Art"].url,
+      description: movieFiltered.description,
+    });
+  }
+
   return (
     <div className="home-container">
-    {movies.map(index =>(
-      <CategoryCard
-        title={index.title}
-        img={index.images['Poster Art'].url}
-        key={index.title}
-      />
-    ))}
+      {movies.map((index) => (
+        <CategoryCard
+          filterToModal={filterToModal}
+          title={index.title}
+          img={index.images["Poster Art"].url}
+          key={index.title}
+        />
+      ))}
 
+      <div className="modal">
+        <h2>{modalState.title}</h2>
+        <img src={modalState.img} alt="Poster Art de Movie" />
+        <p>{modalState.description}</p>
+        <span>{modalState.year}</span>
+      </div>
     </div>
   );
 }
