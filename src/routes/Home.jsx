164ca2307b15b97ed4e34c1be0ffe89bd7fca676
fspind71 <fspind@gmail.com>
2023-08-28 15:59:15 -0300

    ...

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-import { DataContext } from "../dataContext/DataContext";
 import { CategoryCard } from "../components/CategoryCard";
 import "../styles/Home.css";
 
